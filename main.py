6f9fb3c99911e6f8806c39d490ff3202dc72788d
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-02-22 14:29:15 +0300

    add menu line

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ today = date.today()
 
 window = Tk()
 window.title("Hour calculation")
-window.geometry('800x600')
+window.geometry('400x300')
+window.option_add("*tearOff", FALSE)
 
 # frame = Frame(window)
 # frame.pack()
@@ -16,6 +17,20 @@ current_date = Label(
    text=f"Date : {today} "
 )
 current_date.pack(anchor=NE, padx=20, pady=20)
+
+main_menu = Menu()
+file_menu = Menu()
+settings_menu = Menu()
+
+file_menu.add_command(label="New")
+file_menu.add_command(label="Save")
+settings_menu.add_command(label="1")
+settings_menu.add_command(label="2")
+
+main_menu.add_cascade(label="File", menu=file_menu)
+main_menu.add_cascade(label="Settings", menu=settings_menu)
+ 
+window.config(menu=main_menu)
 
 
 window.mainloop()
