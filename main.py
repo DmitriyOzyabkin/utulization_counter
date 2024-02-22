3f80c548a6fd88e99da9dbf9d4e6247835ce8ef9
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-02-22 20:56:33 +0300

    Setttings button and settings window

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ today = date.today()
 
 window = Tk()
 window.title("Hour calculation")
-window.geometry('800x600')
+window.geometry('400x300')
 
 # frame = Frame(window)
 # frame.pack()
+
+def open_settings_window():
+   window_settings = Tk()
+   window_settings.title("Settings")
+   window_settings.geometry('250x200')
 
 current_date = Label(
    text=f"Date : {today} "
 )
 current_date.pack(anchor=NE, padx=20, pady=20)
 
+btn_settings = ttk.Button(text = 'Settings', command = open_settings_window)
+btn_settings.place(relx=0.1, rely=0.87)
 
 window.mainloop()
