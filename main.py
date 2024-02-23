6b94d546cd003be9f8bae11f43caf4682abe8f3b
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-02-23 10:39:12 +0300

    Add pck file for vars

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 from datetime import date
-last_labor_hour = 0
-total_labor = 0
+import pickle
+
+file_name = 'data_hour.pck'
+data_file = open(file_name, mode='rb')
+
+hours = pickle.load(data_file)
+
+# initial var
+last_labor_hour = hours['last_labor_hour']
+total_labor_week = hours['total_labor_week']
+total_labor_year = hours['total_labor_year']
+year_goal = hours['year_goal']
+weeks = hours['weeks']
+
+
 
 today = date.today()
 
@@ -31,12 +44,15 @@ def clear():
  
 def get_last_labor_hour():
    global last_labor_hour
-   global total_labor
-   value = 0
-   value = enter_hour.get()     # save entered hours to variable
+   global total_labor_week
+   # value = 0
+   value = enter_hour.get()    # save entered hours to variable
+   print(type(value), value)
    if value.isdigit():
-      last_labor_hour = int(value)
-      total_labor += last_labor_hour
+      value = float(value)
+      last_labor_hour = value
+      print(last_labor_hour)
+      total_labor_week += last_labor_hour
       clear()
    else:
       label = ttk.Label(text='Not digit!!!')
@@ -66,11 +82,27 @@ enter_hour.place(relx=0.6, rely=0.3)
 btn_enter_hour = ttk.Button(text="Enter", command=get_last_labor_hour)
 btn_enter_hour.place(relx=0.65, rely=0.4)
 
+total_labor_year += total_labor_week
+
+# Year and week hour information feild
+
+label_year_goal = ttk.Label(text=f"Year goal: {year_goal}", background="#FFCDD2", foreground="#B71C1C", padding=8)
+label_year_goal.place(relx=0.1, rely=0.1)
+
+label_year_hour = ttk.Label(text=f"Hours at current year: {total_labor_year}", background="#FFCDD2", foreground="#B71C1C", padding=8)
+label_year_hour.place(relx=0.1, rely=0.2)
+
+label_week_goal = ttk.Label(text=f"Week goal: {int(year_goal/weeks)}", background="#ABF0FF", foreground="#B71C1C", padding=8)
+label_week_goal.place(relx=0.1, rely=0.3)
+
+label_week_hour = ttk.Label(text=f"Hours at current week: {total_labor_week}", background="#ABF0FF", foreground="#B71C1C", padding=8)
+label_week_hour.place(relx=0.1, rely=0.4)
 
 
+total_labor_year += total_labor_week
 
 
 window.mainloop()
 
 
-print(total_labor)
+print(total_labor_year, total_labor_week)
